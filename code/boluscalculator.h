b0a3b863a09909e7209400a4fe06d80274229ab6
xHWAHx <123016795+xHWAHx@users.noreply.github.com>
2025-04-02 19:53:00 -0400

    Update boluscalculator.h

--- code/boluscalculator.h
+++ code/boluscalculator.h
@@ -5,7 +5,13 @@
 class BolusCalculator
 {
 public:
-    BolusCalculator();
+    static double calculateBolus(double glucose, double carbs);
+    static double suggestDose();
+    static void overrideDose(double dose);
+    static bool validateBolusInput(double dose);
+    static double calculateCorrectionDose(double glucose, double target, double sensitivity);
+    static double calculateCarbBolus(double carbs, double ratio);
+    static std::pair<double, double> splitBolus(double totalDose, double percentage);
 };
 
 #endif // BOLUSCALCULATOR_H
