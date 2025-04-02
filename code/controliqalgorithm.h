cc39f828cd9bc76b5e171fe179ac9be546bdb17f
xHWAHx <123016795+xHWAHx@users.noreply.github.com>
2025-04-02 19:54:07 -0400

    Update controliqalgorithm.h

--- code/controliqalgorithm.h
+++ code/controliqalgorithm.h
@@ -1,11 +1,16 @@
 #ifndef CONTROLIQALGORITHM_H
 #define CONTROLIQALGORITHM_H
 
+#include <vector>
 
 class ControlIQAlgorithm
 {
 public:
-    ControlIQAlgorithm();
+    static void analyzeGlucoseData(const std::vector<double>& data);
+    static void adjustBasalRate();
+    static void suspendForLowGlucose();
+    static void increaseInsulinForHighGlucose();
+    static bool isGlucoseLevelStable();
 };
 
 #endif // CONTROLIQALGORITHM_H
