fdb664489d58f963826df7c8e69f518b60fd428e
jusvim <justinschoenhofer@gmail.com>
2025-04-02 21:03:08 -0400

    Implemented batteryManager functions

--- code/batterymanager.cpp
+++ code/batterymanager.cpp
@@ -1,6 +1,33 @@
 #include "batterymanager.h"
 
 BatteryManager::BatteryManager()
-{
+	: batteryLevel {1}
+{ }
 
+double BatteryManager::getBatteryLevel(){
+	// if alertLowBattery() is to be used, it should be here
+	return batteryLevel;
+}
+
+void BatteryManager::drainBattery(){ 
+	// Drains battery level by 1%.
+	// Would not exist in a real device.
+	batteryLevel -= 0.01;
+	if (batteryLevel <= 0){
+		batteryLevel = 0;
+		// Needs to call Device to power off
+	}
+}
+
+void BatteryManager::chargeBattery(){
+	// Charges the battery to full instantly
+	batteryLevel = 1;
+}
+
+bool BatteryManager::isBatteryCritical(){
+	return batteryLevel <= criticalValue;
+}
+
+void BatteryManager::alertLowBattery(){
+	// idk what this should do
 }
--- code/batterymanager.h
+++ code/batterymanager.h
@@ -6,6 +6,15 @@ class BatteryManager
 {
 public:
     BatteryManager();
+    double getBatteryLevel();
+	void drainBattery();
+	void chargeBattery(); 
+	bool isBatteryCritical();
+	void alertLowBattery();
+
+private:
+	double batteryLevel;
+    static constexpr double criticalValue = 0.15;
 };
 
 #endif // BATTERYMANAGER_H
