a85b75d8c1f94a078f0e94b1ddbc51d58aa59c5e
jusvim <justinschoenhofer@gmail.com>
2025-04-03 19:35:20 -0400

    Implemented the functions
    Implemented the functions, and made the CGM readings random.

--- code/cgmreader.cpp
+++ code/cgmreader.cpp
@@ -1,6 +1,32 @@
 #include "cgmreader.h"
+#include <iostream>
 
 CGMReader::CGMReader()
-{
+    : previousReading(startAmount)
+    , randomGenerator()
+    , norm(new std::normal_distribution<double>(0, volatility))
+    , timeOfPreviousReading(std::chrono::steady_clock::now())
 
+{
+    CGMConnected = true;
 }
+
+double CGMReader::getCurrentGlucoseLevel(){
+	auto currentTime = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsedDuration {currentTime - timeOfPreviousReading};
+    double elapsedSeconds = elapsedDuration.count() * timeScale;
+
+    double rand = norm->operator()(randomGenerator);
+    //std::cout << "Random normal value: " << rand << "\n";
+
+    double reading = previousReading + meanReversion * (meanGlucose - previousReading) * elapsedSeconds + std::sqrt(elapsedSeconds) * rand;
+
+	timeOfPreviousReading = currentTime;
+	previousReading = reading;
+	return reading;
+}
+
+bool CGMReader::isCGMConnected() {
+	return CGMConnected;
+}
+
--- code/cgmreader.h
+++ code/cgmreader.h
@@ -1,11 +1,30 @@
 #ifndef CGMREADER_H
 #define CGMREADER_H
+#include <chrono>
+#include <random>
+#include <memory>
 
 
 class CGMReader
 {
+// All values are in mmol/L
 public:
     CGMReader();
+	double getCurrentGlucoseLevel();
+	bool isCGMConnected(); // will we use this?
+	void alertCGMDisconnected(); // idk how this would work
+private:
+	bool CGMConnected;
+	double previousReading;
+    std::default_random_engine randomGenerator;
+    std::unique_ptr<std::normal_distribution<double>> norm;
+    std::chrono::time_point<std::chrono::steady_clock> timeOfPreviousReading;
+    static constexpr double meanReversion = 0.0001; // should never go above 1/timeScale
+    static constexpr double meanGlucose = 11;
+    static constexpr double volatility = 0.01;
+    static constexpr double startAmount = 6;
+    static constexpr double timeScale = 3600; // Glucose levels vary over time at the rate of timeScale faster than normal.
+                                             // A timeScale of 1 means real time, 60 means 1 second simulates 1 minute.
 };
 
 #endif // CGMREADER_H
