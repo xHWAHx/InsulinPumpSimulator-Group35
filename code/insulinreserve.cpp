675085816b90bd35e0c42b201ecde08e4e05eabc
jusvim <justinschoenhofer@gmail.com>
2025-04-02 21:37:16 -0400

    Defined functions for InsulinReserve

--- code/insulinreserve.cpp
+++ code/insulinreserve.cpp
@@ -1,6 +1,30 @@
 #include "insulinreserve.h"
 
 InsulinReserve::InsulinReserve()
-{
+	: insulinRemaining {maxAmount}
+{ }
 
+double InsulinReserve::getInsulinRemaining() {
+	return insulinRemaining;
+}
+
+double InsulinReserve::useInsulin(double amount) {
+	// Returns 0 if it was able to deploy the full amount,
+	// otherwise returns the amount deployed
+	if (amount <= insulinRemaining){
+		insulinRemaining -= amount;
+		return 0;
+	} else {
+		double insulinDeployed = insulinRemaining;
+		insulinRemaining = 0;
+		return insulinDeployed;
+	}
+}
+
+bool InsulinReserve::isInsulinLow() {
+	return insulinRemaining <= lowAmount;
+}
+
+void InsulinReserve::refillInsulin() {
+	insulinRemaining = maxAmount;
 }
--- code/insulinreserve.h
+++ code/insulinreserve.h
@@ -6,6 +6,14 @@ class InsulinReserve
 {
 public:
     InsulinReserve();
+	double getInsulinRemaining();
+    double useInsulin(double amount);
+	bool isInsulinLow();
+	void refillInsulin();
+private:
+	double insulinRemaining; // Measured in "units"
+    static constexpr double maxAmount = 300;
+    static constexpr double lowAmount = maxAmount/10;
 };
 
 #endif // INSULINRESERVE_H
