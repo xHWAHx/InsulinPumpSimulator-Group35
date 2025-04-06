2c720cf0173656773cedd0a4211104493b39fb5f
jusvim <justinschoenhofer@gmail.com>
2025-04-05 22:57:09 -0400

    Added missing files
    I forgot to do git add, so they were missing. Also removed mainwindow,
    because device now does that.

---
+++ code/device.cpp
@@ -0,0 +1,53 @@
+#include "device.h"
+#include "ui_device.h"
+
+Device::Device(QWidget *parent)
+    : QMainWindow{parent}
+    , poweredOn(false)
+    , battery(new BatteryManager)
+    , log(new DataLogger)
+    , insulin(new InsulinReserve)
+    , cgm(new CGMReader)
+    , profiles(new Profile)
+    , pump(new PumpController)
+    , interface(new UserInterface(this))
+    , window(new Ui::Device)
+{
+    window->setupUi(this);
+
+    connect(window->powerButton, &QPushButton::released, this, &Device::power);
+
+    interface->hide(); // because device starts powered off
+}
+
+void Device::power(){
+    if (poweredOn || battery->getBatteryLevel() == 0){
+        poweredOn = false;
+        interface->hide();
+        window->powerLabel->setText("Device is powered off");
+    } else {
+        poweredOn = true;
+        interface->show();
+        window->powerLabel->setText("Device is powered on");
+
+        interface->showLoginScreen();
+
+        //Vaguely what will happen:
+
+        //profile currentProfile = profiles.getActiveProfile(); // "profile" being a struct from profile.h
+
+        //while (poweredOn){
+			// safety checks here
+
+			//logEntry entry; // "logEntry" being a struct from datalogger.h
+			//entry.batteryLevel = battery.getBatteryLevel();
+			//entry.glucose = gcm.getCurrentGlucoseLevel();
+			//ControlIQAlgorithm.adjustBasalRate(glucose); // probably should actually be instantiated so the controlIQ can have a reference to the pump
+			//pump.pump();							
+			//entry.insulinRemaining = insulin.getInsulinRemaining();
+			//log.logTick(entry);
+
+            //sleep(1);
+        //}
+    }
+}
---
+++ code/device.h
@@ -0,0 +1,44 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include <QMainWindow>
+#include <unistd.h>
+#include <batterymanager.h>
+#include <datalogger.h>
+#include <insulinreserve.h>
+#include <cgmreader.h>
+#include <profile.h>
+#include <pumpcontroller.h>
+#include <userinterface.h>
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Device; }
+QT_END_NAMESPACE
+
+class Device : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit Device(QWidget *parent = nullptr);
+
+public slots:
+	void power();
+
+signals:
+
+private:
+	bool poweredOn;
+    BatteryManager *battery;
+    DataLogger *log;
+    InsulinReserve *insulin;
+    CGMReader *cgm;
+    Profile *profiles;
+    PumpController *pump;
+    UserInterface *interface;
+    Ui::Device *window;
+
+};
+
+#endif // DEVICE_H
