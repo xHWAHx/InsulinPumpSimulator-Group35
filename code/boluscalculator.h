9d6d93667f6a8237711034d24e59fdc1a8322cd2
xHWAHx <123016795+xHWAHx@users.noreply.github.com>
2025-04-07 22:22:09 -0400

    Added Static Member Variables

--- code/boluscalculator.h
+++ code/boluscalculator.h
@@ -1,17 +1,22 @@
 #ifndef BOLUSCALCULATOR_H
 #define BOLUSCALCULATOR_H
+
 #include <utility>
 
-class BolusCalculator
-{
+class BolusCalculator {
 public:
     static double calculateBolus(double glucose, double carbs);
     static double suggestDose();
     static void overrideDose(double dose);
     static bool validateBolusInput(double dose);
-    static double calculateCorrectionDose(double glucose, double target, double sensitivity);
-    static double calculateCarbBolus(double carbs, double ratio);
-    static std::pair<double, double> splitBolus(double totalDose, double percentage);
+    static double calculateCorrectionBolus(double glucose, double target, double correctionFactor);
+    static double calculateCarbBolus(double carbs, double carbRatio);
+    static double calculateTotalBolus(double glucose, double carbs, double target);
+    static std::pair<double, double> splitBolus(double total, double percentage);
+
+private:
+    static double overriddenDose;
+    static bool doseOverridden;
 };
 
 #endif // BOLUSCALCULATOR_H
