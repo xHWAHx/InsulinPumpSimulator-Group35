a669f9b734d3eb7d2275f5e5697252c585ee1ad6
jusvim <justinschoenhofer@gmail.com>
2025-04-21 15:58:58 -0400

    Fixed controlIQ behaviour and logging
    Fixed the bugs caused by my last commit.
    
    Now every change in basal rate is logged and
    nothing is logged on every tick.
    
    Fixed some incorrect edge cases in pumping rates.

--- code/controliqalgorithm.cpp
+++ code/controliqalgorithm.cpp
@@ -5,24 +5,30 @@
 #include <numeric>
 #include <iostream>
 
+double ControlIQAlgorithm::currentRate = 0;
+
 void ControlIQAlgorithm::analyzeGlucoseData(double glucose, DataLogger* logger, PumpController* pump) {
 
     // loads active profile data
     Profile profile = Profile::getActiveProfile();
     double target = profile.getTargetGlucose();
-    double currentRate = profile.getBasalRate();
+    double profileRate = profile.getBasalRate();
 
     if (glucose <= 3.9) {
         adjustBasalRate(pump, 0);
         logger->logEvent("Warning", "Low glucose detected. Basal rate pumping suspended.");
-    } else if (glucose > target) {
-        adjustBasalRate(pump, currentRate); // fine-tunes basal rate for stable glucose
+    } else if ((glucose > target) and (currentRate == 0)) {
+        adjustBasalRate(pump, profileRate);
         logger->logEvent("Info", "Glucose stable. Resumed basal rate pumping.");
+    } else if ((currentRate != 0) and (currentRate != profileRate)) {
+        adjustBasalRate(pump, profileRate);
+        logger->logEvent("Info", "Basal rate changed manually to " + QString::number(profileRate) + ".");
     }
 }
 
 void ControlIQAlgorithm::adjustBasalRate(PumpController* pump, double rate) {
     if (pump) {
+        currentRate = rate;
         pump->adjustBasalRate(rate);
     }
 }
--- code/controliqalgorithm.h
+++ code/controliqalgorithm.h
@@ -10,6 +10,7 @@ class ControlIQAlgorithm {
 public:
     static void analyzeGlucoseData(double data, DataLogger* logger, PumpController* pump);
     static void adjustBasalRate(PumpController* pump, double rate);
+    static double currentRate;
     //static void suspendForLowGlucose(PumpController* pump);
     //static void increaseInsulinForHighGlucose(PumpController* pump, double currentRate);
     //static bool isGlucoseLevelStable(double glucose, double target);
