f886f24a6c1919576e69a17d0ce4801a779f9146
KamalYassin <117047318+KamalYassin@users.noreply.github.com>
2025-04-26 03:15:32 -0400

    alert.h

--- code/alert.h
+++ code/alert.h
@@ -9,16 +9,25 @@ namespace Ui {
 class Alert;
 }
 
-class Alert : public QWidget
-{
+/**
+ * @brief The Alert class displays warning or error alerts to the user.
+ */
+class Alert : public QWidget {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for Alert screen.
+     * @param parent Optional parent widget.
+     */
     explicit Alert(UserInterface *parent = nullptr);
+
+    /**
+     * @brief Destructor for Alert screen.
+     */
     ~Alert();
 
-    //void showAlert(const QString &title, const QString &body);
-
+    /// Predefined alert types
     static constexpr int BATTERY_LOW = 1;
     static constexpr int INSULIN_LOW = 2;
     static constexpr int CGM_DISCONNECTED = 3;
@@ -26,12 +35,24 @@ public:
     static constexpr int GLUCOSE_LOW = 4;
     static constexpr int GLUCOSE_HIGH = 5;
 
+    /**
+     * @brief Raises a new alert of a specific type.
+     * @param type Type of the alert.
+     * @param interface Pointer to the user interface.
+     * @param logger Pointer to the event logger.
+     */
     static void raise(int type, UserInterface *interface, DataLogger *logger);
+
+    /**
+     * @brief Resets and clears a specific alert type.
+     * @param type Type of the alert to reset.
+     */
     static void reset(int type);
 
 private:
     Ui::Alert *ui;
 
+    /// Keeps track of which alerts are currently active.
     static QHash<int, bool> raisedAlerts;
 };
 
