b2ec47ed91793b0357d5f0c050faff414eab3e94
KamalYassin <117047318+KamalYassin@users.noreply.github.com>
2025-04-26 03:14:51 -0400

    login.h

--- code/login.h
+++ code/login.h
@@ -7,43 +7,50 @@ namespace Ui {
 class Login;
 }
 
-class Login : public QWidget
-{
+/**
+ * @brief The Login class manages PIN entry for unlocking the device.
+ */
+class Login : public QWidget {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for Login screen.
+     * @param parent Optional QWidget parent.
+     */
     explicit Login(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for Login screen.
+     */
     ~Login();
+
+signals:
+    /**
+     * @brief Emitted when the correct PIN is entered and device unlocks.
+     */
+    void deviceUnlocked();
 
 private slots:
     void on_button1_clicked();
-
     void on_button2_clicked();
-
     void on_button3_clicked();
-
     void on_button4_clicked();
-
     void on_button5_clicked();
-
     void on_button6_clicked();
-
     void on_button7_clicked();
-
     void on_button8_clicked();
-
     void on_button9_clicked();
-
     void on_button0_clicked();
-
     void on_buttonOk_clicked();
 
-signals:
-    void deviceUnlocked();
 private:
     Ui::Login *ui;
 
 protected:
+    /**
+     * @brief Captures keyboard input for entering the PIN.
+     */
     void keyPressEvent(QKeyEvent *event) override;
 };
 
